3872ce7d96bea55e58486ba0d4c60bfa7f42bab7
Filip <filiptrajkoski2005@gmail.com>
2025-05-25 16:37:03 +0200

    Update 4.cpp

--- OOP-exercises from second midterm 2024/4.cpp
+++ OOP-exercises from second midterm 2024/4.cpp
@@ -171,101 +171,102 @@ int main() {
 
 -----------------------------------------
 
-//     #include <iostream>
+// #include <iostream>
 
 // using namespace std;
 
-// class Library {
+// class Museum {
 // protected:
 //     string name;
 //     string city;
 //     double price;
-//     bool weekend;
+//     int workHours;
 
 // public:
-//     Library(const string &name = "", const string &city = "", double price = 0, bool weekend = false) {
+//     Museum(const string &name = "", const string &city = "", double price = 0, int workHours = 0) {
 //         this->name = name;
 //         this->city = city;
 //         this->price = price;
-//         this->weekend = weekend;
+//         this->workHours = workHours;
 //     }
 
 //     virtual void printDetail() = 0;
 
-//     virtual double calculateMembershipCardCost() = 0;
+//     virtual double calculateTicketCost() = 0;
+
+//     int getWorkHours() const {
+//         return workHours;
+//     }
+//     virtual ~Museum() = default;
 // };
 
-// //AcademicLibrary и NationalLibrary
-// class AcademicLibrary : public Library {
+// //ScientificMuseum и ArtMuseum
+
+// class ScientificMuseum : public Museum {
 // private:
 //     bool something;
 //     int number;
 
 // public:
-//     AcademicLibrary(const string &name = "", const string &city = "", double price = 0, bool weekend = false,
-//                     bool something = false, int number = 0) : Library(name, city, price, weekend), number(number) {
+//     ScientificMuseum(const string &name = "", const string &city = "", double price = 0, int workHours = 0,
+//                      bool something = false, int number = 0) : Museum(name, city, price, workHours) {
 //         this->something = something;
 //         this->number = number;
 //     }
 
-//     double calculateMembershipCardCost() override {
+//     double calculateTicketCost() override {
 //         double price = this->price;
-//         if (something) {
-//             price = price * 1.24;
+//         if (this->something) {
+//             price = price * 1.12;
 //         }
 //         price = price + (number * 6);
 //         return price;
 //     }
 
 //     void printDetail() override {
-//         //[Ime na biblioteka] - (Academic) [Grad] [Broj na spec. kolekcii] [Cena na clenarina]
-//         cout << name << " - (Academic) " << city << " " << number << " " << calculateMembershipCardCost() << endl;
+//         //[Ime na muzej] - (Scientific) [Grad] [Broj na mult. prezentacii] [Cena na karta]
+//         cout << name << " - (Scientific) " << city << " " << number << " " << calculateTicketCost() << endl;
 //     }
 // };
 
-// class NationalLibrary : public Library {
+// class ArtMuseum : public Museum {
 // private:
 //     bool something;
 //     int number;
 
 // public:
-//     NationalLibrary(const string &name = "", const string &city = "", double price = 0, bool weekend = false,
-//                     bool something = false, int number = 0) : Library(name, city, price, weekend), number(number) {
+//     ArtMuseum(const string &name = "", const string &city = "", double price = 0, int workHours = 0,
+//               bool something = false, int number = 0) : Museum(name, city, price, workHours) {
 //         this->something = something;
 //         this->number = number;
 //     }
 
-//     double calculateMembershipCardCost() override {
+//     double calculateTicketCost() override {
 //         double price = this->price;
-//         if (something) {
-//             price = price * 0.93;
+//         if (this->something) {
+//             price = price * 0.82;
 //         }
-//         price = price + (number * 15);
+//         price = price + (number * 3);
 //         return price;
 //     }
 
 //     void printDetail() override {
-//         //[Ime na biblioteka] - (National) [Grad] [Broj na rakopisi] [Cena na clenarina]
-//         cout << name << " - (National) " << city << " " << number << " " << calculateMembershipCardCost() << endl;
-//     }
-
-//     bool getSomething() {
-//         return something;
+//         //[Ime na muzej] - (Art) [Grad] [Amaterski izlozbi] [Cena na karta]
+//         cout << name << " - (Art) " << city << " " << something << " " << calculateTicketCost() << endl;
 //     }
 // };
 
-// int findMostExpensiveNationalLibrary(Library **l, int n) {
+// int findCheapestScientificMuseum(Museum **m, int n) {
 //     int index = -1;
 //     for (int i = 0; i < n; i++) {
-//         NationalLibrary *temp = dynamic_cast<NationalLibrary *>(l[i]);
-//         if (temp) {
+//         if (ScientificMuseum *temp = dynamic_cast<ScientificMuseum *>(m[i])) {
 //             if (index == -1) {
 //                 index = i;
 //             } else {
-//                 if (l[index]->calculateMembershipCardCost() < temp->calculateMembershipCardCost()) {
+//                 if (temp->calculateTicketCost() < m[index]->calculateTicketCost()) {
 //                     index = i;
-//                 } else if (l[index]->calculateMembershipCardCost() == temp->calculateMembershipCardCost() && temp->
-//                            getSomething()) {
+//                 } if (temp->calculateTicketCost() == m[index]->calculateTicketCost() && temp->getWorkHours() > m[
+//                                index]->getWorkHours()) {
 //                     index = i;
 //                 }
 //             }
@@ -281,13 +282,13 @@ int main() {
 //     cin >> testCase >> n;
 //     cin.ignore();
 
-//     Library **m = new Library *[n];
+//     Museum **m = new Museum *[n];
 
 //     for (int i = 0; i < n; ++i) {
 //         string name;
 //         string city;
 //         float base_price;
-//         bool weekend_working;
+//         int working_hours;
 
 //         cin >> type;
 //         cin.ignore();
@@ -295,25 +296,25 @@ int main() {
 //         getline(cin, city);
 //         cin >> base_price;
 //         cin.ignore();
-//         cin >> weekend_working;
+//         cin >> working_hours;
 //         cin.ignore();
 
 //         if (type == 1) {
-//             bool open_cooperation;
-//             int specialized_articles;
+//             bool interactive_shows;
+//             int multimedia_pres;
 
-//             cin >> open_cooperation >> specialized_articles;
+//             cin >> interactive_shows >> multimedia_pres;
 //             cin.ignore();
 
-//             m[i] = new AcademicLibrary(name, city, base_price, weekend_working, open_cooperation, specialized_articles);
+//             m[i] = new ScientificMuseum(name, city, base_price, working_hours, interactive_shows, multimedia_pres);
 //         } else {
-//             bool cultural_program;
-//             int national_articles;
+//             bool amateur_shows;
+//             int original_artwork;
 
-//             cin >> cultural_program >> national_articles;
+//             cin >> amateur_shows >> original_artwork;
 //             cin.ignore();
 
-//             m[i] = new NationalLibrary(name, city, base_price, weekend_working, cultural_program, national_articles);
+//             m[i] = new ArtMuseum(name, city, base_price, working_hours, amateur_shows, original_artwork);
 //         }
 //     }
 
@@ -321,22 +322,22 @@ int main() {
 //         cout << "Abstract and child classes OK" << endl;
 //     } else if (testCase == 2) {
 //         for (int i = 0; i < n; i++) {
-//             cout << m[i]->calculateMembershipCardCost() << endl;
+//             cout << m[i]->calculateTicketCost() << endl;
 //         }
-//         cout << "calculateMembershipCardCost method OK" << endl;
+//         cout << "calculateTicketCost method OK" << endl;
 //     } else if (testCase == 3) {
 //         for (int i = 0; i < n; i++) {
 //             m[i]->printDetail();
 //         }
 //         cout << "printDetail method OK" << endl;
 //     } else if (testCase == 4) {
-//         int most_expensive_nat_lib_index = findMostExpensiveNationalLibrary(m, n);
-//         if (most_expensive_nat_lib_index >= 0) {
-//             m[most_expensive_nat_lib_index]->printDetail();
+//         int cheapest_sci_museum_index = findCheapestScientificMuseum(m, n);
+//         if (cheapest_sci_museum_index >= 0) {
+//             m[cheapest_sci_museum_index]->printDetail();
 //         } else {
-//             cout << "National Library not found in the array!" << endl;
+//             cout << "Scientific Museum not found in the array!" << endl;
 //         }
-//         cout << "findMostExpensiveNationalLibrary method OK" << endl;
+//         cout << "findCheapestOnlineOrder method OK" << endl;
 //     }
 
 
@@ -348,3 +349,4 @@ int main() {
 
 //     return 0;
 // }
+
